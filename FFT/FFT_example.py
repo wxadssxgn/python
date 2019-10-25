bcb10a5ff0a8f7a873fdadefbf672ee0c49d6c36
wxadssxgn <19493864@qq.com>
2019-10-25 18:25:53 +0800

    fft

---
+++ FFT/FFT_example.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+tp = 0.75
+t0 = 5
+N = 100
+ExcitationFunction = []
+time = np.linspace(start=0, stop=10, num=N)
+ts = (time[1]-time[0])
+fs = 1/ts
+
+for dtime in time:
+    ExcitationFunction_tmp = (-(dtime-t0)/tp)*np.exp(0.5-np.power(dtime-t0, 2)/(2*np.power(tp, 2)))
+    ExcitationFunction.append(ExcitationFunction_tmp)
+
+plt.figure(1)
+plt.plot(time, ExcitationFunction)
+plt.title('Time Domain')
+plt.xlabel('time/ns')
+
+y = np.fft.fft(ExcitationFunction)
+freq = np.fft.fftfreq(N, ts)
+
+plt.figure(2)
+plt.plot(freq[0: int(N/2)], abs(y[0: int(N/2)]))
+plt.title('Frequency Domain')
+plt.ylabel('Amplitude')
+plt.xlabel('Frequency/GHz')
+plt.show()
